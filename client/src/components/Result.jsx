8726b2cdcd99e92e79e1dd2cd7e9e2e8343b56c3
kaleem7832 <kaleem.nalband@gmail.com>
2020-08-11 18:28:25 +0530

    Excel Export added

--- client/src/components/Result.jsx
+++ client/src/components/Result.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import TableToExcel from "@linways/table-to-excel";
 import axios from "axios";
 import moment from "moment";
 class Result extends Component {
@@ -7,11 +7,20 @@ class Result extends Component {
     super(props);
     this.state = {
       tasks: [],
+      name: "one",
     };
   }
   componentWillReceiveProps(props) {
     // properties changed
-    console.log("search filter", props.search.programmer);
+    let name = [];
+    name.push(props.search.programmer);
+    name.push(props.search.project);
+    name.push(props.search.startdate.slice(0, 5));
+    name.push(props.search.enddate.slice(0, 5));
+    let fname = name.filter(function (item) {
+      if (item) return item;
+    });
+    this.setState({ name: fname.join("_") });
     axios
       .get("/tasks/filter/", {
         params: {
@@ -28,34 +37,58 @@ class Result extends Component {
         console.log(error);
       });
   }
+  excelExport = () => {
+    let name = "TaskTacker_" + this.state.tasks[0].date;
+    TableToExcel.convert(document.getElementById("result"), {
+      name: this.state.name + ".xlsx",
+      sheet: {
+        name: "Sheet 1",
+      },
+    });
+  };
+
   render() {
     return (
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Project</th>
-            <th>Programmer</th>
-            <th>Task</th>
-            <th>Start Time</th>
-            <th>End Time</th>
-            <th>Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.tasks.map((task) => (
+      <React.Fragment>
+        <button
+          className={
+            this.state.tasks.length > 0
+              ? "btn btn-primary bottom-right"
+              : "hide"
+          }
+          onClick={this.excelExport}
+        >
+          Excel Export
+        </button>
+        <table className="table" id="result">
+          <thead>
             <tr>
-              <td>{task.date}</td>
-              <td>{task.project}</td>
-              <td>{task.programmer}</td>
-              <td>{task.task}</td>
-              <td>{moment(task.starttime).format("LT")}</td>
-              <td>{task.endtime ? moment(task.endtime).format("LT") : "-"}</td>
-              <td>{task.status}</td>
+              <th>Date</th>
+              <th>Project</th>
+              <th>Programmer</th>
+              <th>Task</th>
+              <th>Start Time</th>
+              <th>End Time</th>
+              <th>Status</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {this.state.tasks.map((task) => (
+              <tr>
+                <td>{task.date}</td>
+                <td>{task.project}</td>
+                <td>{task.programmer}</td>
+                <td>{task.task}</td>
+                <td>{moment(task.starttime).format("LT")}</td>
+                <td>
+                  {task.endtime ? moment(task.endtime).format("LT") : "-"}
+                </td>
+                <td>{task.status}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
     );
   }
 }
